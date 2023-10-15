bb82833f8d10b7e9285f1638bc78decf0eafbf36
STTGL <bayramlit7@gmail.com>
2023-10-15 11:18:44 -0700

    6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,22 +1,15 @@
-#include <stdio.h>
-
-/**
- * main - Entry point of the program
- *
- * Description: Prints the sizes of various data types
- *
- * Return: Always 0 (success)
- */
-int main(void)
-{
-    printf("Size of a char:" sizeof(char));
-    printf("Size of an int:" sizeof(int));
-    printf("Size of a long:" sizeof(long));
-    printf("Size of a long long:" sizeof(long long));
-    printf("Size of a float:" sizeof(float));
-    printf("Size of a double:" sizeof(double));
-    printf("Size of a long double:" sizeof(long double);
-
-    return (0);
-}
-
+#include <stdio.h>                                                                                                                  
+                                                                                                                                       
+/**                                                                                                                                 
+* main - Entry point                                                                                                               
+* Return: Always 0 (Success)                                                                                                       
+*/                                                                                                                                 
+int main(void)                                                                                                                      
+{                                                                                                                                   
+      printf("Size of a char: %lu byte(s)\n", sizeof(char));                                                                      
+      printf("Size of an int: %lu byte(s)\n", sizeof(int));                                                                       
+      printf("Size of a long int: %lu byte(s)\n", sizeof(long int));                                                              
+      printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));                                                     
+        printf("Size of a float: %lu byte(s)\n", sizeof(float));                                                                    
+       return (0);                                                                                                                 
+  }
