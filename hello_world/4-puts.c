19fbce95998d7c104220b146d566347b2267c40f
STTGL <bayramlit7@gmail.com>
2023-10-15 10:05:53 -0700

    4c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
-int main() {
-puts("Programming is like building a multilingual puzzle");
-return 0; }
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a message and returns 0
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
