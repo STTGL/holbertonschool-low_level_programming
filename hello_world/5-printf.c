01dd34504d18a36815ca3147331873a11cff8fcc
STTGL <bayramlit7@gmail.com>
2023-10-15 10:13:11 -0700

    5c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the message with proper grammar
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("but the outcome is a piece of art,\n");
+	return (0);
+}
+
