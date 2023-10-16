8bfad62e17132ecea70157c0cff12ab0cbdaa38c
STTGL <bayramlit7@gmail.com>
2023-10-16 04:40:09 -0700

    1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -12,7 +12,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n%10>5)
 	{printf("Last digit of %d is and is greater than 5\n", n);}
-	if else (n%10==0)
+	else if (n%10==0)
 	{printf ("Last digit of %d is and is 0", n);}
 	else {printf("Last digit of %d is and is less than 6 and not 0", n);}
 	return (0);
