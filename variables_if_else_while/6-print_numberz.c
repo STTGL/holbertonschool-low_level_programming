6374586669064627f2d085a856d7178817df00f5
STTGL <bayramlit7@gmail.com>
2023-10-20 04:43:57 -0700

    6

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -11,7 +11,7 @@ int main(void)
 
         for (digit = 0; digit < 10; digit++)
         {
-                putchar("%i", digit);
+                putchar(digit);
         }
         putchar('\n');
         return (0);
