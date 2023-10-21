77f056f765a93cf7d47e46ce49c0986aad3aa521
STTGL <bayramlit7@gmail.com>
2023-10-21 09:47:26 -0700

    6

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - this program prints numbers
+ * main - Entry point. Prints single-digit numbers from 0 to 9, followed by a new line.
+ *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-        int digit;
+    char digit;
 
-        for (digit = 0; digit < 10; digit++)
-        {
-                putchar('0' + digit);
-        }
-        putchar('\n');
-        return (0);
+    for (digit = '0'; digit <= '9'; digit++) {
+        putchar(digit);
+    }
+    
+    putchar('\n');
+    
+    return (0);
 }
+
