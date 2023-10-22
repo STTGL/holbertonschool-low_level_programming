2d7789119e8c60140e2846d2bd21e53c73645f47
STTGL <bayramlit7@gmail.com>
2023-10-22 03:22:50 -0700

    last

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two given numbers.
+ * @a: argument for adding
+ * @b: argument for adding
+ * Return: sum of the given numbers
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
