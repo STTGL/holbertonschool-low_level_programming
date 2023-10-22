066ae17d757c2249405f961c6f5af487aa8efc50
STTGL <bayramlit7@gmail.com>
2023-10-22 03:07:53 -0700

    9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * times_table - generates times table
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int i, j;
+	int mult;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			mult = i * j;
+
+			if (j > 0 && mult <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+
+			}
+			else if (j > 0 && mult > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+}
+		_putchar('\n');
+	}
+}
