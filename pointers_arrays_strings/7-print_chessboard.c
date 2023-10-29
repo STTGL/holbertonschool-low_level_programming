69efc2347cda8d5cbc4ae68bcf353b4033dcb949
STTGL <bayramlit7@gmail.com>
2023-10-29 12:01:27 -0700

    7

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_chessboard - Print a chessboard
+ * @a: A 2D array representing the chessboard
+ *
+ * Prints the chessboard represented by a 2D array.
+ */
+
+void print_chessboard(char (*a)[8])
+{
+int row, col;
+
+for (row = 0; row < 8; row++)
+{
+for (col = 0; col < 8; col++)
+{
+putchar(a[row][col]);
+if (col < 7)
+{
+putchar(' ');
+}
+else
+{
+putchar('\n');
+}
+}
+}
+}
