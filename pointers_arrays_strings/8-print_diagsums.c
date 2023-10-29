b3b3dfb1112418fde6c8d65fcc55e405eee2ebfd
STTGL <bayramlit7@gmail.com>
2023-10-29 12:46:33 -0700

    8

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
- * print_diagsums - Print the sum of the two diagonals of a square matrix
+ * print_diagsums - Print the sum of the two diagonals
  * @a: The square matrix (2D array)
  * @size: The size of the matrix (number of rows or columns)
  *
- * This function calculates and prints the sum of the main diagonal and
- * anti-diagonal of a square matrix of integers.
+ * This function calculates and prints a sum
  */
-void print_diagsums(int *a, int size) {
-    int main_diag_sum = 0;
-    int anti_diag_sum = 0;
-    int i = 0;
+void print_diagsums(int *a, int size)
+{
+int main_diag_sum = 0;
+int anti_diag_sum = 0;
+int i = 0;
 
-    for (i = 0; i < size; i++) {
-        main_diag_sum += a[i * size + i];
-        anti_diag_sum += a[i * size + (size - 1 - i)];
-    }
+for (i = 0; i < size; i++)
+{
+main_diag_sum += a[i * size + i];
+anti_diag_sum += a[i * size + (size - 1 - i)];
+}
 
-    printf("%d\n", main_diag_sum);
-    printf("%d\n", anti_diag_sum);
+printf("%d\n, %d\n", main_diag_sum, anti_diag_sum);
 }
