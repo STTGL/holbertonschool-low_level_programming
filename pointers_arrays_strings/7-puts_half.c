04d80150d2ce68d04f7a6893cf032fb0098f52d7
STTGL <bayramlit7@gmail.com>
2023-10-29 01:03:59 -0700

    78

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * puts_half - is a function that prints half of a string,
+ * followed by a new line
+ *
+ * @str: string argument
+ */
+
+void puts_half(char *str)
+{
+	int len;
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		int i;
+
+		for (i = len / 2; i <= len - 1; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+
+	else
+	{
+		int j;
+
+		for (j = (len + 1) / 2; j <= len - 1; j++)
+		{
+			_putchar(str[j]);
+		}
+	}
+
+	_putchar('\n');
+}
