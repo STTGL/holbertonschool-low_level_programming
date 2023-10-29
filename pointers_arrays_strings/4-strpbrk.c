6694cbfc2415ea70f1216c3f8335c8e8a6c1662a
STTGL <bayramlit7@gmail.com>
2023-10-29 11:42:28 -0700

    34

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * _strpbrk - Search a string for any of a set of bytes
+ * @s: The string to search
+ * @accept: The set of bytes to search for
+ *
+ * Return: A pointer
+ */
+char *_strpbrk(char *s, char *accept)
+{
+int i, j;
+
+for (i = 0; s[i] != '\0'; i++) 
+{
+for (j = 0; accept[j] != '\0'; j++) 
+{
+if (s[i] == accept[j])
+{
+return (&s[i]);  /* Return a pointer to the matching byte in s */
+}
+}
+}
+
+return (NULL);
+}
