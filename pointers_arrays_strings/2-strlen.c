bfacae39b1c8bb69b957ad98778224126d52857e
STTGL <bayramlit7@gmail.com>
2023-10-29 01:43:34 -0700

    23

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,17 @@
 #include "main.h"
 #include <string.h>
 
+/**
+ * _strlen - Calculates the length of a string.
+ * @s: The input string.
+ *
+ * Return: The length of the string.
+ */
 int _strlen(char *s)
-{       int p;
-	p=strlen(s);
-	return(p);
+{
+	int p;
+
+	p = strlen(s);
+	return (p);
 }
+
--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,13 +1,17 @@
-#include"main.h"
+#include "main.h"
 #include <stdio.h>
 
+/**
+ * _puts - Prints a string followed by a new line.
+ * @str: The string to be printed.
+ */
 void _puts(char *str)
 {
 	while (*str)
 	{
 		putchar(*str);
 		str++;
-	
 	}
 	putchar('\n');
 }
+
