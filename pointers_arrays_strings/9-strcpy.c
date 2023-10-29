520682070433546af9e0b04dd9124bb488b72c2f
STTGL <bayramlit7@gmail.com>
2023-10-29 01:09:55 -0700

    9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copy a string including the null byte
+ * @dest: Destination buffer
+ * @src: Source string
+ *
+ * Return: Pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
+
