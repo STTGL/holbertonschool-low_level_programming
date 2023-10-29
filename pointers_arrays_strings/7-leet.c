ad98a6c271384b55dcaef843fbc7637c8e2f3aa3
STTGL <bayramlit7@gmail.com>
2023-10-29 07:53:29 -0700

    12

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -8,19 +8,23 @@
  * Return: A pointer to the modified string
  */
 
-char *leet(char *str) {
+char *leet(char *str)
+{
 char *original = "aAeEoOtTlL";
 char *replacements = "443370711";
 int i, j;
 
-for (i = 0; str[i] != '\0'; i++) {
-for (j = 0; original[j] != '\0'; j++) {
-if (str[i] == original[j]) {
+for (i = 0; str[i] != '\0'; i++)
+{
+for (j = 0; original[j] != '\0'; j++)
+{
+if (str[i] == original[j])
+{
 str[i] = replacements[j];
 break;
 }
 }
 }
 
-    return (str);
+return (str);
 }
