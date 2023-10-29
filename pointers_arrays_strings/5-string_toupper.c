3d6cea3f3c8f9d82892e8abb3f5529ae5654cee0
STTGL <bayramlit7@gmail.com>
2023-10-29 07:19:54 -0700

    some

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - is a function
+ * @c: argument
+ * Return: char
+ *
+ */
+
+char *string_toupper(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if (c[i] >= 'a' && c[i] <= 'z')
+		{
+			c[i] -= 32;
+		}
+	}
+	return (c);
+}
---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * cap_string- is a function
+ *
+ * @str: string argument
+ * Return: capitalized string
+ */
+
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ((str[i] >= 'a' && str[i] <= 'z')
+			       && (str[i - 1] == ' '
+				       || str[i - 1] == '\t'
+				       || str[i - 1] == '\n'
+				       || str[i - 1] == '.'
+				       || str[i - 1] == ','
+				       || str[i - 1] == ';'
+				       || str[i - 1] == '!'
+				       || str[i - 1] == '?'
+				       || str[i - 1] == '"'
+				       || str[i - 1] == '('
+				       || str[i - 1] == ')'
+				       || str[i - 1] == '{'
+				       || str[i - 1] == '}'))
+		{
+			str[i] -= offset;
+		}
+	}
+	i = 0;
+	if (str[i] >= 'a' && str[i] <= 'z')
+	{
+		str[i] -= 32;
+	}
+	return (str);
+}
