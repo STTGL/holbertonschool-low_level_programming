f46cd1d490dabcab0e9e867cbb68c0f9580c2244
STTGL <bayramlit7@gmail.com>
2023-11-05 07:40:23 -0800

    last

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,24 +1,26 @@
 #include "main.h"
 
+/**
+ * _pow_recursion - calculates x raised to the power of y
+ *
+ * @x: the base
+ * @y: the exponent
+ *
+ * Return: the result of x^y
+ */
 int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
 
-{
-if (y < 0)
-{
-return (-1);
-}
-if (y==0)
-{
-return (1);
-}
-x*=x;
-y--;
-if (y > 1)
-{
-_pow_recursion (x, y);
-}
-if (y == 1)
-{
-return (x);
-}
-}
