1669b342f7f9879d35b5eb2c70162a4fbc8fe7c9
STTGL <bayramlit7@gmail.com>
2023-11-05 07:19:19 -0800

    oof

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _puts_recursion - prints a string
@@ -10,12 +11,12 @@ void _puts_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_putchar(s[0]);
+		putchar(s[0]);
 		_puts_recursion(s + 1);
 
 	}
 	else
 	{
-		_putchar('\n');
+		putchar('\n');
 	}
 }
