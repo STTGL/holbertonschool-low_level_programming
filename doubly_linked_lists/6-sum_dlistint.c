e482ff03540119b74364d46c4df84b48e5934bc0
STTGL <bayramlit7@gmail.com>
2023-12-10 08:16:57 -0800

    1

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint -  sum of all the data (n) of a dlistint_t linked list.
+ * @head: pointer to head node
+ * Return: sum of all the data or 0 if list is empty
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum;
+
+	sum = 0;
+	if (head == NULL)
+		return (0);
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
